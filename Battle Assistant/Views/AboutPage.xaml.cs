9fe568845abd2757bfc24e6cd80e4c03c4eb5cb0
Archie Hill <archiedhill@gmail.com>
2022-05-31 15:28:06 +0100

    Added in the app version into the about page

--- Battle Assistant/Views/AboutPage.xaml.cs
+++ Battle Assistant/Views/AboutPage.xaml.cs
@@ -23,9 +23,19 @@ namespace Battle_Assistant.Views
     /// </summary>
     public sealed partial class AboutPage : Page
     {
+        public string Version
+        {
+            get
+            {
+                var version = Windows.ApplicationModel.Package.Current.Id.Version;
+                return string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
+            }
+        }
+
         public AboutPage()
         {
             this.InitializeComponent();
+            DataContext = this;
         }
     }
 }
